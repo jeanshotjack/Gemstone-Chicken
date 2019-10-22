121917182aa9b4a15fcb896e316e495fe9017af7
jeanshotjack <dshalenkova@gmail.com>
2019-10-21 22:46:53 -0400

    define variables, attach random values to variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+var win = 0;
+var loss = 0;
+var num1 = [Math.floor(Math.random() * 12) + 1];
+var num2 = [Math.floor(Math.random() * 12) + 1];
+var num3 = [Math.floor(Math.random() * 12) + 1];
+var num4 = [Math.floor(Math.random() * 12) + 1];
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+$(document).ready(function () {
+
+var randomNum = [Math.floor(Math.random() * 120) + 19];
+$("#randomNum").append(randomNum);
+console.log(randomNum);
+});
