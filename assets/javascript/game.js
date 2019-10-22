78b8984bd341b00194fe4ed18f2ec3fe9e98b003
jeanshotjack <dshalenkova@gmail.com>
2019-10-21 23:22:18 -0400

    add user total to html, add click function to crystal, psuedocode rest

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var num1 = [Math.floor(Math.random() * 12) + 1];
 var num2 = [Math.floor(Math.random() * 12) + 1];
 var num3 = [Math.floor(Math.random() * 12) + 1];
 var num4 = [Math.floor(Math.random() * 12) + 1];
+var user = 0;
 console.log(num1);
 console.log(num2);
 console.log(num3);
@@ -11,7 +12,24 @@ console.log(num4);
 
 $(document).ready(function () {
 
-var randomNum = [Math.floor(Math.random() * 120) + 19];
-$("#randomNum").append(randomNum);
-console.log(randomNum);
+    var computer = [Math.floor(Math.random() * 120) + 19];
+    $("#computer").append(computer);
+    console.log(computer);
+
+    // set reset function
+    // new random number, append new number, assign new crystal numbers, reset and append user total
+
+    // set winner/loser function
+    // add wins/losses, signal winner, reset 
+
+
+
+    $("#num1").on("click", function () {
+        user = user + num1;
+        user + num1; // fix so it actually adds instead of just sticking the number on
+        $("#user").append(user);
+    });
+    // repeat for all numbers
+
+    // if else statements for winner and loser
 });
