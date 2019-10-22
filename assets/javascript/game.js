77139902f77e237da6be926f144a2f526d4fcfc8
jeanshotjack <dshalenkova@gmail.com>
2019-10-22 14:28:06 -0400

    fix loser function, winner still broken

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,12 +13,12 @@ console.log(num4);
 $(document).ready(function () {
 
     var computer = [Math.floor(Math.random() * 101) + 19];
-    $("#computer").append(computer);
+    $("#computer").text(computer);
     console.log(computer);
 
     function reset() {
-        var computer = [Math.floor(Math.random() * 101) + 19];
-        $("#computer").append(computer);
+        computer = [Math.floor(Math.random() * 101) + 19];
+        $("#computer").text(computer);
         console.log(computer);
         num1 = Math.floor(Math.random() * 12) + 1;
         num2 = Math.floor(Math.random() * 12) + 1;
@@ -28,45 +28,57 @@ $(document).ready(function () {
         $("#user").text(user);
     }
 
-    function winner () {
+    function winner() {
         win++;
         $("#win").text(win);
         reset();
     }
 
-
-    function loser () {
+    function loser() {
         loss++;
         $("#loss").text(loss);
         reset();
+    }
+
+    function scoreChecker() {
+        if (user === computer) {
+            winner();
+            console.log("Winner");
+        }
+
+        else if (user > computer) {
+            loser();
+            console.log("LOSER!");
+        }
     }
 
     $("#num1").on("click", function () {
         user = user + num1;
         console.log(user);
         $("#user").text(user);
+        scoreChecker();
     });
     $("#num2").on("click", function () {
         user = user + num2;
         console.log(user);
         $("#user").text(user);
+        scoreChecker();
     });
+
     $("#num3").on("click", function () {
         user = user + num3;
         console.log(user);
         $("#user").text(user);
+        scoreChecker();
+
     });
+
     $("#num4").on("click", function () {
         user = user + num4;
         console.log(user);
         $("#user").text(user);
+        scoreChecker();
     });
 
-    if (user === computer) {
-        winner();
-    }
-
-    else if (user > computer) {
-        loser();
-    }
+    
 });
