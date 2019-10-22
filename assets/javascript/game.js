136456bde5e83c827c94de4bce4994e90105b12f
jeanshotjack <dshalenkova@gmail.com>
2019-10-22 09:49:15 -0400

    fix numbers adding as strings issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 var win = 0;
 var loss = 0;
-var num1 = [Math.floor(Math.random() * 12) + 1];
-var num2 = [Math.floor(Math.random() * 12) + 1];
-var num3 = [Math.floor(Math.random() * 12) + 1];
-var num4 = [Math.floor(Math.random() * 12) + 1];
+var num1 = Math.floor(Math.random() * 12) + 1;
+var num2 = Math.floor(Math.random() * 12) + 1;
+var num3 = Math.floor(Math.random() * 12) + 1;
+var num4 = Math.floor(Math.random() * 12) + 1;
 var user = 0;
 console.log(num1);
 console.log(num2);
@@ -12,7 +12,7 @@ console.log(num4);
 
 $(document).ready(function () {
 
-    var computer = [Math.floor(Math.random() * 120) + 19];
+    var computer = [Math.floor(Math.random() * 101) + 19];
     $("#computer").append(computer);
     console.log(computer);
 
@@ -26,10 +26,27 @@ $(document).ready(function () {
 
     $("#num1").on("click", function () {
         user = user + num1;
-        user + num1; // fix so it actually adds instead of just sticking the number on
-        $("#user").append(user);
+        // user + num1; // fix so it actually adds instead of just sticking the number on
+        console.log(user);
+        $("#user").text(user);
     });
-    // repeat for all numbers
+    $("#num2").on("click", function () {
+        user = user + num2;
+        // user + num1; // fix so it actually adds instead of just sticking the number on
+        console.log(user);
+        $("#user").text(user);
+    });
+    $("#num3").on("click", function () {
+        user = user + num3;
+        // user + num1; // fix so it actually adds instead of just sticking the number on
+        console.log(user);
+        $("#user").text(user);
+    });
+    $("#num4").on("click", function () {
+        user = user + num4;
+        console.log(user);
+        $("#user").text(user);
+    });
 
     // if else statements for winner and loser
 });
